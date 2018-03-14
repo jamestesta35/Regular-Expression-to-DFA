7cb95ddc426cd04fcea503c9ae16aeeb688b0fa7
James Testa <james_testa@student.uml.edu>
2018-03-14 16:06:23 -0400

    Added command line arguments for regex

--- regex_main.cpp
+++ regex_main.cpp
@@ -5,12 +5,55 @@
 #include "finiteMachine.h"
 #include "expression.h"
 
-int main(){
+void help(){
+  std::cout << "This is the help menu for regex_main" << std::endl;
+}
+
+void usage(){
+  std::cout << "Usage: regex_main [options] [regular expression]" << std::endl;
+}
+
+int main(int var_num, char** vars){
   DIR *dp;
   struct dirent *ep;
   std::string regex, file, input;
   std::ifstream infile;
   int lineCount;
+  bool help_var = false;
+  bool usage_var = false;
+
+  for(int i = 1; i < var_num; ++i){
+    if((string)vars[i] == "-h" || (string)vars[i] == "-help" || (string)vars[i] == "-H"){
+      help_var = true;
+    } else if ((string)vars[i] == "-u"){
+      usage_var = true;
+    } else {
+      if(regex.empty()){
+	regex = string(vars[i]);
+      } else {
+	cout << "Too many variables. Exiting." << std::endl;
+	return 2;
+      }
+    }
+  }
+
+  if(usage_var){
+    usage();
+  }
+
+  if(help_var){
+    help();
+  }
+
+  if(usage_var || help_var){
+    return 1;
+  }
+
+  if(regex.empty()){
+    std::cout << "Expected a regular expression" << endl;
+    return 3;
+  }
+  
   std::cout << "Type the regular expression:" << endl;
   std::cin >> regex;
   regex = ".*" + regex + ".*";
