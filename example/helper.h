32bf7020f0a2209ddba1e01a29a7867b658eede6
James Testa <james_testa@student.uml.edu>
2018-04-28 11:51:41 -0400

    Adding sample output file

--- example/helper.h
+++ example/helper.h
@@ -1,3 +1,7 @@
 //This is an example header file
 
 //This is for testing purposes
+
+//The file sample_json.txt in this directory should find lines that have the phrase "finiteMachine"
+
+Helper_File
